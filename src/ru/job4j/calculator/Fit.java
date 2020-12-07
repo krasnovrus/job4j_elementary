b405616ec6fad241bf4248095a90d78d68e45199
krasnovrus <krasnov121russpmi>
2020-12-07 15:38:56 +0300

    6. Идеальный вес [#422409]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 175;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 175 is " + man);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Woman 175 is " + woman);
+    }
+
+}
