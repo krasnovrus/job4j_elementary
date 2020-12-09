eb437d32a8e8b712093095222755761869f3f99c
krasnovrus <krasnov121russpmi>
2020-12-09 14:06:02 +0300

    1. Модульные тесты. [#422442]

--- test/ru/job4j/converter/ConverterTestTest.java
+++ test/ru/job4j/converter/ConverterTestTest.java
@@ -8,17 +8,17 @@ public class ConverterTestTest {
 
     @Test
     public void whenConvert140RblThen2Euro() {
-        int in_e = 140; //Входное значение//
-        int expected_e = 2; //Ожидаемое значение//
-        int out_e = Converter.rubleToEuro(in_e); //Результат работы программы//
-        Assert.assertEquals(expected_e, out_e);
+        int inE = 140; //Входное значение//
+        int expectedE = 2; //Ожидаемое значение//
+        int outE = Converter.rubleToEuro(inE); //Результат работы программы//
+        Assert.assertEquals(expectedE, outE);
     }
 
     @Test
     public void whenConvert180RblThen3Dollar() {
-        int in_d = 180; //Входное значение//
-        int expected_d = 3; //Ожидаемое значение//
-        int out_d = Converter.rubleToDollar(in_d); //Результат работы программы//
-        Assert.assertEquals(expected_d, out_d);
+        int inD = 180; //Входное значение//
+        int expectedD = 3; //Ожидаемое значение//
+        int outD = Converter.rubleToDollar(inD); //Результат работы программы//
+        Assert.assertEquals(expectedD, outD);
     }
 }
