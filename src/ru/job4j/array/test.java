e89707d8cca36fc57c8654d63b1a20185e15bb50
krasnovrus <krasnov121russpmi>
2021-01-12 22:21:44 +0300

    6.0.2. Заполнение массива. [#422390]

---
+++ src/ru/job4j/array/test.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class test {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        array[0] = 5;
+        array[1] = 3;
+        array[2] = 4;
+        array[3] = 9;
+        array[4] = 2;
+        System.out.println(array[2]);
+    }
+}
