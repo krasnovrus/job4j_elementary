6f408969a5ff1a863e6ea1516c66a868792b24a4
krasnovrus <krasnov121russpmi>
2021-02-03 12:15:05 +0300

    3. Объединить два отсортированных массива

--- src/ru/job4j/Merge.java
+++ src/ru/job4j/Merge.java
@@ -4,20 +4,37 @@ public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
 
-        int count = 0;
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
-            count++;
-        }
-        for (int j = 0; j < right.length; j++) {
-            rsl[count++] = right[j];
+        int i=0, j=0;
+
+        for (int k=0; k<rsl.length; k++) {
+
+            if (i > left.length-1) {
+                int a = right[j];
+                rsl[k] = a;
+                j++;
+            }
+            else if (j > right.length-1) {
+                int a = left[i];
+                rsl[k] = a;
+                i++;
+            }
+            else if (left[i] < right[j]) {
+                int a = left[i];
+                rsl[k] = a;
+                i++;
+            }
+            else {
+                int b = right[j];
+                rsl[k] = b;
+                j++;
+            }
         }
 
         return rsl;
     }
 
     public static void main(String[] args) {
-        int[] rsl = merge(new int[]{4, 5, 6, 7, 8}, new int[]{3, 4, 5, 6, 7, 8});
+        int[] rsl = merge(new int[]{21, 23, 24, 40, 75, 76, 78, 77, 900, 2100, 2200, 2300, 2400, 2500}, new int[]{10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000});
         for (int i = 0; i < rsl.length; i++) {
             System.out.print(rsl[i] + " ");
         }
